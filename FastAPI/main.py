80b2a518c3673620bcc943db0a1def12059ef30c
grom311 <grom311@yandex.ru>
2022-05-28 14:46:26 +0300

    full_stack clean

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth, todos, users, address, todo
+from routers import auth, todos
 from starlette.staticfiles import StaticFiles
 app = FastAPI()
 
@@ -10,8 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth.router)
-app.include_router(todo.router)
 app.include_router(todos.router)
-app.include_router(users.router)
-app.include_router(address.router)
-
